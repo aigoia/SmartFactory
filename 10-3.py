2916767adf0d205fc8af3ccea6f8e74a6d91eb4e
aigoia <oidoicompany@gmail.com>
2025-01-05 04:41:42 +0900

    cleaning

---
+++ 10-3.py
@@ -0,0 +1,28 @@
+class Health:
+    def __init__(self, name, hp):
+        self._name = name
+        self.__hp = 50
+        self.hp = 100 if hp > 100 else 1 if hp < 1 else hp 
+
+    def exercise(self, times=1):
+        self.__hp = self.__hp + times if self.__hp + times < 100 else 100
+        self.hp = self.hp + times if self.hp + times < 100 else 100
+        print(f"{self._name} private hp: {self.__hp}")
+        print(f"{self._name} public hp: {self.hp}")
+
+    def drink_alcohol(self, times=1):
+        self.__hp = self.__hp - times if self.__hp - times > 1 else 1
+        self.hp = self.hp - times if self.hp - times > 1 else 1
+        print(f"{self._name} private hp: {self.__hp}")
+        print(f"{self._name} public hp: {self.hp}")
+        
+    def set_hp(self, hp):
+        self.__hp = hp
+        
+test = Health("jone", 50)
+test.exercise(5)
+test.__hp = 10
+test.hp = 10
+test.drink_alcohol(1)
+test.set_hp(10)
+test.exercise(5)
