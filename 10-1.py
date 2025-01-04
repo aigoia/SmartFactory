67d0b2a8d9206571a7f46ccc4f9505f93309684b
aigoia <oidoicompany@gmail.com>
2025-01-05 03:36:31 +0900

    math class

---
+++ 10-1.py
@@ -0,0 +1,24 @@
+class MathTest:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+        
+    def add(self):
+        return self.first + self.second
+    
+    def sub(self):
+        return self.first - self.second
+    
+    def mul(self):
+        return self.first * self.second
+    
+    def div(self):
+        return self.first / self.second if self.second != 0 else None
+    
+
+test = MathTest(2, 4)
+
+print(test.add())
+print(test.sub())
+print(test.mul())
+print(test.div())
