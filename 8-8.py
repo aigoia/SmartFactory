c09ba3df4046db20eb993319b58d59db27046f8d
aigoia <oidoicompany@gmail.com>
2025-01-04 20:01:16 +0900

    mean

--- 8-8.py
+++ 8-8.py
@@ -1,3 +1,5 @@
+import statistics
+
 score = {"korean":  [83, 90, 88], "english": [92, 79, 86], "math":  [88, 86, 94]}
 
 mean_korean = 0
@@ -5,16 +7,10 @@ for i in score["korean"]:
     mean_korean = mean_korean + i
 mean_korean = mean_korean / len(score["korean"]) 
 
-mean_english = 0
-for i in score["english"]:
-    mean_english = mean_english + i
-mean_english = mean_english / len(score["english"])
+mean_english = sum(score["english"]) / len(score["english"])
 
-mean_math = 0
-for i in score["math"]:
-    mean_math =   mean_math + i
-mean_math = mean_math / len(score["math"])
+mean_math = statistics.mean(score["math"])
 
-print(f"math mean {mean_korean}")
+print(f"korean mean {mean_korean}")
 print(f"english mean {mean_english}")
-print(f"korean mean {mean_math}")
+print(f"math mean {mean_math}")
