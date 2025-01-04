f04c2bf4ddaa855ce4d72a6ce5b5d0ccfe2912f8
aigoia <oidoicompany@gmail.com>
2025-01-05 04:31:31 +0900

    bug fix

--- 10-2.py
+++ 10-2.py
@@ -2,22 +2,22 @@ class Health:
     def __init__(self, name, hp):
         self._name = name
         self.__hp = 50
-        self.hp = 100 if hp > 100 else 0 if hp < 0 else hp 
+        self.hp = 100 if hp > 100 else 1 if hp < 1 else hp 
 
     def exercise(self, times=1):
         self.__hp = self.__hp + times if self.__hp + times < 100 else 100
         self.hp = self.hp + times if self.hp + times < 100 else 100
-        print(f"{self._name} private hp {self.__hp}")
-        print(f"{self._name} public hp {self.hp}")
+        print(f"{self._name} private hp: {self.__hp}")
+        print(f"{self._name} public hp: {self.hp}")
 
     def drink_alcohol(self, times=1):
-        self.__hp = self._hp - times if self.__hp + times < 1 else 1
-        self.hp = self.hp - times if self.hp + times < 100 else 100
-        print(f"{self._name} private hp {self.__hp}")
-        print(f"{self._name} public hp {self.hp}")
+        self.__hp = self.__hp - times if self.__hp - times > 1 else 1
+        self.hp = self.hp - times if self.hp - times > 1 else 1
+        print(f"{self._name} private hp: {self.__hp}")
+        print(f"{self._name} public hp: {self.hp}")
         
 test = Health("jone", 50)
-test.exercise(4)
-test.__hp = 0
-test.hp = 0
-test.exercise(4)
+test.exercise(5)
+test.__hp = 1
+test.hp = 1
+test.drink_alcohol(1)
