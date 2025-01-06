154561c0534efb395d7a8e73762d20471d5f0b21
aigoia <oidoicompany@gmail.com>
2025-01-06 15:15:32 +0900

    use del

--- python/7/7-1.py
+++ python/7/7-1.py
@@ -1,6 +1,7 @@
 rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
 
 print(rainbow[2-1])
+print(len(rainbow))
 
 sorted_rainbow = rainbow.copy()
 sorted_rainbow.sort()
@@ -17,6 +18,11 @@ pop_rainbow.pop(2)
 pop_rainbow.pop(2)
 pop_rainbow.pop(2)
 print(pop_rainbow)
+
+# 제거할게 연속적인걸 활용하는 방법
+del_rainbow = rainbow.copy()
+del del_rainbow[2:6]
+print(del_rainbow)
 
 # 때문에 지우는걸 반대로 생각해서 남은걸 넣는 다는 걸로 해결한다
 deleted_rainbow = [rainbow[0], rainbow[1], rainbow[6]]
