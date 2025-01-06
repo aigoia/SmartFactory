a5038001e6a65fc83d60aa416b652fac1ecbcede
aigoia <oidoicompany@gmail.com>
2025-01-06 15:00:44 +0900

    fix code

--- Python/7/7-1.py
+++ Python/7/7-1.py
@@ -2,14 +2,23 @@ rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
 
 print(rainbow[2-1])
 
-sorted_rainbow = rainbow
+sorted_rainbow = rainbow.copy()
 sorted_rainbow.sort()
 print(sorted_rainbow)
 
-append_rainbow = rainbow
+append_rainbow = rainbow.copy()
 append_rainbow.append('crimson')
 print(append_rainbow)
 
+# 이건 코드가 너무 복잡하다
+pop_rainbow = rainbow.copy()
+pop_rainbow.pop(2)
+pop_rainbow.pop(2)
+pop_rainbow.pop(2)
+pop_rainbow.pop(2)
+print(pop_rainbow)
+
+# 때문에 지우는걸 반대로 생각해서 남은걸 넣는 다는 걸로 해결한다
 deleted_rainbow = [rainbow[0], rainbow[1], rainbow[6]]
 print(deleted_rainbow)
 
