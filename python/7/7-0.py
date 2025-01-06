1b5a8aa39d575a2d77b721d60daf49388d49a02a
aigoia <oidoicompany@gmail.com>
2025-01-06 20:31:09 +0900

    cleaning

--- python/7/7-0.py
+++ python/7/7-0.py
@@ -1,10 +1,21 @@
-
-# dict1 = {}
-dit1 = dict()
-
-print(dict1)
+dict1 = dict()
 
 dict1 = {
     "name": "홍길동",
     "age": 20
 }
+
+dict1["birthday"] = "0000"
+print(dict1)
+
+fruits = {"apple": "사과", "banana": "바나나"}
+
+print(fruits.get("peach", "복숭아"))
+# -> 존재하는 키로 검색시 기본값을 설정
+
+fruits.update({"grapes": "포도"})
+print(fruits)
+
+print(fruits.keys())
+print(fruits.values())
+print(fruits.items())
