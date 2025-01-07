335111e5e1c103c68a5e140131206aefbdca8179
aigoia <oidoicompany@gmail.com>
2025-01-07 20:35:48 +0900

    cleaning

--- python/8/8-0.py
+++ python/8/8-0.py
@@ -1,12 +1,2 @@
-a, b = 5, 8
-max_value = a if a > b else b
-print(max_value)
-
-score = 91 
-grade = "A" if score >= 90 else ("B" if score >= 60 else "c")
-
-new_grade = "A" if score >= 90 else "B" \
-                if score >= 60 else "c" 
-            
-print(grade)
-print(new_grade)
+numbers = [x**2 for x in range(1, 6) if x & 2 == 0]
+print(numbers)
