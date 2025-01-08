99d94548cee79316543c2e0982af9721565c6d70
aigoia <oidoicompany@gmail.com>
2025-01-08 19:36:17 +0900

    cleaning

--- python/9/9-0.py
+++ python/9/9-0.py
@@ -1,3 +1,14 @@
-print(set("hello"))
-print(list("hello"))
-print(tuple("hello"))
+def six():
+    return 6 
+
+def plus(a, b):
+    return a + b
+
+int_six = 6
+test_six = six()
+
+print(six)
+print(test_six)
+
+test_plus = plus(2, six())
+print(test_plus)
