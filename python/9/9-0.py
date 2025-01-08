dca4dd556a47f6020383f4ecb775e83f9a127bcb
aigoia <oidoicompany@gmail.com>
2025-01-08 20:20:17 +0900

    cleanign

--- python/9/9-0.py
+++ python/9/9-0.py
@@ -1,28 +1,32 @@
-def six():
-    return 6 
+# def six():
+#     return 6 
 
-def two():
-    return 2
+# def two():
+#     return 2
 
-def plus(a, b):
-    return a + b
+# def plus(a, b):
+#     return a + b
 
-def void():
-    pass
+# def void():
+#     pass
 
-print(void())
+# print(void())
 
-int_six = 6
-def_six = six()
+# int_six = 6
+# def_six = six()
 
-print(int_six)
-print(def_six)
+# print(int_six)
+# print(def_six)
 
-test_plus = plus(2, six())
-print(test_plus)
+# test_plus = plus(2, six())
+# print(test_plus)
 
-test_minus = six() - two()
-def_plus = plus(six(), two())
+# test_minus = six() - two()
+# def_plus = plus(six(), two())
 
-print(test_minus)
-print(def_plus)
+# print(test_minus)
+# print(def_plus)
+
+def intro(name, age, city):
+    print(f"이름은 {name}이고 나이는 {age}이고, 사는 곳은 {city}입니다")
+    
