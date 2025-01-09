94af2477078493b4ddb48a739ffacd03a2408a7b
aigoia <oidoicompany@gmail.com>
2025-01-09 16:40:27 +0900

    cleaning

--- python/9/9-0.py
+++ python/9/9-0.py
@@ -1,9 +1,9 @@
-plus = lambda a, b: a + b
-
-plus_one = lambda a, b: {
-    a + 1,
-    b + 1
-}
-
-print(plus(1, 2))
-print(plus_one(1, 2))
+def factorial(n):
+    print("n >>", n)
+    
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print(factorial(3))
